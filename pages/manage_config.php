6765cf7063175551d1c9fac1abac74d7c4bc3f4f
Olivier <olivier.legras@mass-n.com>
2013-06-18 13:34:39 +0200

    Clean + Fix

--- pages/manage_config.php
+++ pages/manage_config.php
@@ -18,50 +18,75 @@ print_manage_menu();
 <br />
 
 <form action="<?php echo plugin_page( 'manage_config_edit' )?>" method="post">
-<table align="center" class="width75" cellspacing="1">
+	<table align="center" class="width75" cellspacing="1">
 
-<tr <?php echo helper_alternate_class( )?>>
-	<td><?php echo plugin_lang_get( 'activate' ) ?></td>
-	<td>
-		<label>
-			<input type="radio" name="activate" value="<?php echo ON ?>" <?php echo ( ( $t_activate == ON )  ? ' checked="checked"' : NULL ) ?>/>
-			<?php echo plugin_lang_get( 'yes' ) ?>
-		</label>
-	</td>
-	<td>
-		<label>
-			<input type="radio" name="activate" value="<?php echo OFF ?>"<?php echo ( ( $t_activate == OFF ) ? ' checked="checked"' : NULL ) ?>/>
-			<?php echo plugin_lang_get( 'no' ) ?>
-		</label>
-	</td>
-</tr>
-<tr <?php echo helper_alternate_class( )?>>
-        <td><?php echo plugin_lang_get( 'host' ) ?></td>
-        <td colspan="2"><label><input type="text" name="host" cols="10" rows="1" value="<?php echo string_attribute( $t_host ) ?>"></label></td>
-</tr>
-<tr <?php echo helper_alternate_class( )?>>
-        <td><?php echo plugin_lang_get( 'port' ) ?></td>
-        <td colspan="2"><input type="text" name="port" cols="10" rows="1" value="<?php echo string_attribute( $t_port ) ?>"></td>
-</tr><tr <?php echo helper_alternate_class( )?>>
-        <td><?php echo plugin_lang_get( 'user' ) ?></td>
-        <td colspan="2"><input type="text" name="user" cols="10" rows="1" value="<?php echo string_attribute( $t_user ) ?>"></td>
-</tr><tr <?php echo helper_alternate_class( )?>>
-        <td><?php echo plugin_lang_get( 'password' ) ?></td>
-        <td colspan="2"><input type="text" name="password" cols="10" rows="1" value="<?php echo string_attribute( $t_password ) ?>"></td>
-</tr><tr <?php echo helper_alternate_class( )?>>
-        <td><?php echo plugin_lang_get( 'vhost' ) ?></td>
-        <td colspan="2"><input type="text" name="vhost" cols="10" rows="1" value="<?php echo string_attribute( $t_vhost ) ?>"></td>
-</tr><tr <?php echo helper_alternate_class( )?>>
-        <td><?php echo plugin_lang_get( 'exchange' ) ?></td>
-        <td colspan="2"><input type="text" name="exchange" cols="10" rows="1" value="<?php echo string_attribute( $t_exchange ) ?>"></td>
-</tr>
-<tr>
-	<td class="center" width="100%" colspan="3">
-		<input type="submit" class="button" value="submit" />
-	</td>
-</tr>
+		<tr <?php echo helper_alternate_class( )?>>
+			<td><?php echo plugin_lang_get( 'activate' ) ?></td>
+			<td>
+				<label>
+					<input type="radio" name="activate" value="<?php echo ON ?>" <?php echo ( ( $t_activate == ON )  ? ' checked="checked"' : NULL ) ?>/>
+					<?php echo plugin_lang_get( 'yes' ) ?>
+				</label>
+			</td>
+			<td>
+				<label>
+					<input type="radio" name="activate" value="<?php echo OFF ?>"<?php echo ( ( $t_activate == OFF ) ? ' checked="checked"' : NULL ) ?>/>
+					<?php echo plugin_lang_get( 'no' ) ?>
+				</label>
+			</td>
+		</tr>
+		<tr <?php echo helper_alternate_class( )?>>
+			<td>
+				<?php echo plugin_lang_get( 'host' ) ?></td>
+			<td colspan="2">
+				<label>
+					<input type="text" name="host" cols="10" rows="1" value="<?php echo string_attribute( $t_host ) ?>">
+				</label>
+			</td>
+		</tr>
+		<tr <?php echo helper_alternate_class( )?>>
+			<td><?php echo plugin_lang_get( 'port' ) ?></td>
+			<td colspan="2">
+				<label>
+					<input type="text" name="port" cols="10" rows="1" value="<?php echo string_attribute( $t_port ) ?>">
+				</label>
+			</td>
+		</tr><tr <?php echo helper_alternate_class( )?>>
+			<td><?php echo plugin_lang_get( 'user' ) ?></td>
+			<td colspan="2">
+				<label>
+					<input type="text" name="user" cols="10" rows="1" value="<?php echo string_attribute( $t_user ) ?>">
+				</label>
+			</td>
+		</tr><tr <?php echo helper_alternate_class( )?>>
+			<td><?php echo plugin_lang_get( 'password' ) ?></td>
+			<td colspan="2">
+				<label>
+					<input type="text" name="password" cols="10" rows="1" value="<?php echo string_attribute( $t_password ) ?>">
+				</label>
+			</td>
+		</tr><tr <?php echo helper_alternate_class( )?>>
+			<td><?php echo plugin_lang_get( 'vhost' ) ?></td>
+			<td colspan="2">
+				<label>
+					<input type="text" name="vhost" cols="10" rows="1" value="<?php echo string_attribute( $t_vhost ) ?>">
+				</label>	
+			</td>
+		</tr><tr <?php echo helper_alternate_class( )?>>
+			<td><?php echo plugin_lang_get( 'exchange' ) ?></td>
+			<td colspan="2">
+				<label>
+					<input type="text" name="exchange" cols="10" rows="1" value="<?php echo string_attribute( $t_exchange ) ?>">
+				</label>
+			</td>
+		</tr>
+		<tr>
+			<td class="center" width="100%" colspan="3">
+				<input type="submit" class="button" value="<?php echo plugin_lang_get( 'submit' ) ?>" />
+			</td>
+		</tr>
 
-</table>
+	</table>
 </form>
 
 <?php
